d3b368501c2348a0ebb080f65334ae5d8d73e438
Jamie Still <jamie@jamiestill.com>
2022-12-05 23:09:15 -0600

    Layout updates for non-hero sections

---
+++ scripts.js
@@ -0,0 +1,7 @@
+console.info(`If you're looking at this code...welcome! I've left this site
+unminifed and unobfuscated so you can browse my work.
+
+Like what you see? Hiring?
+Contact me at https://www.linkedin.com/in/jamiestill :)  
+
+jamie@jamiestill.com`);
