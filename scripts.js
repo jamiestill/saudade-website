c8dcd505e96bc5ab7ee0db103d5c7a3a1662d326
Jamie Still <jamie@jamiestill.com>
2022-12-07 19:39:06 -0600

    Fig captions w/ GPS coordinates

--- scripts.js
+++ scripts.js
@@ -6,3 +6,33 @@ Like what you see? Hiring?
 Contact me at jamie@jamiestill.com or see https://www.linkedin.com/in/jamiestill :)  
 
 `);
+
+   
+/*
+ Insert Google Earth links for figcaptions with GPS coordinates
+ */
+
+// Get the figcaptions
+let figcaptions = document.querySelectorAll('figcaption .lat-long');
+if (figcaptions) {
+
+    figcaptions.forEach(figcaption => { 
+        // Retrieve URL
+        let latLong = figcaption.innerText;
+
+        // Build Google Earth link
+        let googleEarthLink = encodeURI("https://earth.google.com/web/search/" + latLong);
+
+        // Add back text into DOM
+        figcaption.innerHTML = `
+            <a href="${googleEarthLink}" 
+            target="_blank" rel="noopener noreferrer"
+            title="Where on earth was this photo taken?">
+                ${latLong}
+            </a>`;
+        }
+    );
+}
+
+    
+    
