639c6259113ed648de8c8e7d8609feaafc9c784f
Jamie Still <jamie@jamiestill.com>
2022-12-15 00:19:30 -0600

    New console.log message

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-console.info(`
-If you're looking at this code...welcome! I've left this site unminifed and unobfuscated so you can browse my work.
-
-Like what you see? Hiring?
-
-Contact me at jamie@jamiestill.com or see https://www.linkedin.com/in/jamiestill :)  
-
-`);
+/**
+ *  console.log Hiring message -- Hire me! 💼
+ */
+console.info(
+	`\n%cIf you're looking at this code...welcome!\nI've left this site unminifed and unobfuscated so you can browse my work.\nLike what you see? Hiring? Contact me at jamie@jamiestill.com or see\nhttps://www.linkedin.com/in/jamiestill \n👨‍💻\n`,
+	`
+	line-height: 2;
+	font-family: courier;
+	font-size: 14px;`
+);
