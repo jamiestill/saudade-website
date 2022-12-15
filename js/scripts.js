553a2c65c4239b04221e01bff55ac288bbfb5526
Jamie Still <jamie@jamiestill.com>
2022-12-15 01:00:55 -0600

    Emoji

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@
  *  console.log Hiring message -- Hire me! 💼
  */
 console.info(
-	`\n%cIf you're looking at this code...welcome!\nI've left this site unminifed and unobfuscated so you can browse my work.\nLike what you see? Hiring? Contact me at jamie@jamiestill.com or see\nhttps://www.linkedin.com/in/jamiestill \n👨‍💻\n`,
+	`\n%cIf you're looking at this code...welcome!\nI've left this site unminifed and unobfuscated so you can browse my work.\nLike what you see? Hiring? Contact me at jamie@jamiestill.com or see\nhttps://www.linkedin.com/in/jamiestill \n%c💻☕\n`,
 	`
 	line-height: 2;
 	font-family: courier;
-	font-size: 14px;`
+	font-size: 14px;`,
+	`font-size: 32px;`
 );
